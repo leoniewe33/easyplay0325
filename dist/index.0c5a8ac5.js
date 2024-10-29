06063f5c4143c17f11d1a0bff1b44b49656d2b7e
stjnrenn <junhao.ren@student.hs-anhalt.de>
2024-10-29 13:51:25 +0100

    bundle with parcel and fix the location of stern.png

---
+++ dist/index.0c5a8ac5.js
@@ -0,0 +1,19 @@
+//Funktionalität der Navigation
+document.getElementById("menuButton").addEventListener("click", function() {
+    document.body.classList.toggle("drawer-open");
+});
+document.addEventListener("click", function(event) {
+    var drawer = document.getElementById("drawer");
+    var menuButton = document.getElementById("menuButton");
+    if (!drawer.contains(event.target) && !menuButton.contains(event.target)) document.body.classList.remove("drawer-open");
+});
+document.getElementById("searchButton").addEventListener("click", function() {
+    var searchBar = document.getElementById("podcast-search");
+    searchBar.classList.toggle("visible");
+    if (searchBar.classList.contains("visible")) document.getElementById("search-title").focus();
+});
+document.getElementById("closeButton").addEventListener("click", function() {
+    document.body.classList.remove("drawer-open");
+});
+
+//# sourceMappingURL=index.0c5a8ac5.js.map
